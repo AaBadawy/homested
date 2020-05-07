c9b7239c142d70422c2ecafd09a80dd3a320355e
ahmed-Badawy-fciah <ahmedbadawy.fcia@gmail.com>
2020-05-07 23:52:57 +0200

    make article.vue component

---
+++ resources/assets/js/components/navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">Articles</a>
+        </div>
+    </div>
+</template>
